a20b45a880a874d8d99b42c4bae8e31c810a6a51
Roberto <robertomartinonwg@gmail.com>
2021-07-21 16:17:02 +0200

    Esercizio completato

---
+++ js/main.js
@@ -0,0 +1,79 @@
+/* Esercizio numero 1
+// 1 - Inserisci un numero. 
+var numero = parseInt(prompt("inserisci un numero"));
+
+
+// 2 - se è pari stampa il numero.
+if ((numero % 2) == 0) {
+    console.log(numero);
+//se è dispari stampa il numero successivo.
+} else {
+    console.log (numero + 1);
+}
+*/
+
+/*
+//Esercizio numero 2
+//Generatore di “nomi cognomi” casuali: 
+//prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.
+var nomi = ["Luigi", "Antonio", "Alberto", "Diego"];
+var cognomi = ["Alfio", "Fiorenzi", "Mariso", "Maradona"];
+var nomiCasuali;
+var cognomiCasuali;
+
+for (var i = 0; i < nomi.length; i++) {
+    nomiCasuali = Math.floor(Math.random() * nomi.length);
+    cognomiCasuali = Math.floor(Math.random() * cognomi.length);
+    console.log((nomi[nomiCasuali]) + (" ") + (cognomi[cognomiCasuali]));
+}
+*/
+
+
+
+
+
+
+
+/* Esercizio numero 3
+//Crea un array di numeri interi
+var numeriInteri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var somma = 0;
+//Fai la somma di tutti gli elementi che sono in posizione dispari.
+
+for (var i = 1; i < numeriInteri.length ; i = i + 2) {
+    somma += numeriInteri[i];
+    console.log(numeriInteri[i]);
+}
+
+console.log("La somma degli elementi in posizione dispari è " + somma);*/
+
+
+//Esercizio numero 4 - SOLUZIONE NON DINAMICA
+//Crea due array che hanno un numero di elementi diversi.
+/*
+var nomeListaUno = ["Alfredo", "Diego", "Matteo"];
+var nomeListaDue = ["Giusy", "Giulia", "Antonia", "Irene", "Lucrezia"];
+//Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+nomeListaUno.push("Roberto", "Alessio");
+console.log(nomeListaUno);
+*/
+//Esercizio numero 4 - SOLUZIONE DINAMICA
+//Crea due array che hanno un numero di elementi diversi.
+var nomeListaUno = ["Alfredo", "Diego", "Matteo"];
+var nomeListaDue = ["Giusy", "Giulia", "Antonia", "Irene", "Lucrezia"];
+
+//Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+var differenza = (nomeListaDue.length) - (" ") - (nomeListaUno.length);
+var numeroInterazioni = parseInt(differenza);
+var nomiAggiunti;
+
+for (var i = 0; i < numeroInterazioni; i++) {
+    nomiAggiunti = prompt("inserisci nome");
+    nomeListaUno.push(nomiAggiunti);
+}
+console.log(nomeListaUno);
+
+
+
